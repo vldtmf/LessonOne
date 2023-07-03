59c54ac05892bcb45d8b67261f30cb812b8439a7
vldtmf <vdovaclico@gmail.com>
2023-07-03 22:08:25 +0300

    example003_summ

---
+++ Examples/Example003_Summ/Program.cs
@@ -0,0 +1,5 @@
+﻿// Написать программу на языке C# для Win и Mac OS, в которую
+// пользователь вводит два целых числа и получает сумму
+int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
