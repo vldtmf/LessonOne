6fc36e9d7be841fd5afc148457d00b01dfc17052
vldtmf <vdovaclico@gmail.com>
2023-07-03 22:30:11 +0300

    example004_div

---
+++ Examples/Example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿// Написать программу на языке C# для Win и Mac OS, в которую
+// пользователь вводит два целых числа и получает частное 
+double numberA = 10;
+double numberB = 3;
+Console.WriteLine(numberA / numberB);
